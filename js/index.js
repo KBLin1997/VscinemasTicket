354d5c5f3b47af161170394506b10085589358c5
KBLin1997 <kaibooooooo1997@gmail.com>
2024-06-17 15:00:54 +0800

    新增第一頁彈出視窗

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(function(){
+
+    $('button').click(function(){
+      $('.mask').css('display', 'block');
+    })
+  
+    $('.bi').click(function(){
+      $('.mask').hide();
+    })
+  
+    $('.mask').click(function(e){
+      $(this).removeAttr('style');
+      console.log(e.eventPhase);
+    })
+  
+    $('.popup').click(function(e){
+      e.stopPropagation();
+      // return false;
+      console.log(e.eventPhase);
+    })
+      
+  
+  })
